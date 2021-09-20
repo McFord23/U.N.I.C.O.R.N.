57d0b86c099631e5ff389208bc4cd349ee845d36
McFord <McFord@ukr.net>
2021-09-20 22:37:00 +0300

    Добавлена механика захвата объектов ✓ Настроена прорисовака камеры (0.01 - 100) ✓ Коллайдер игрока теперь состоит из капсулы (тело) и сферы (голова)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,9 +12,13 @@ public class Player : MonoBehaviour
     GameObject view;
     public float sensitivity = 100f;
     Vector3 inputDir = Vector3.zero;
-    Vector3 viewDir = Vector3.zero;
     public float smoothTime = 0.1f;
-    
+
+    bool takeMode = false;
+    public int maxTakeDistance;
+    public float force;
+    public LayerMask part;
+    Rigidbody objectRB;
 
     void Start()
     {
@@ -25,6 +29,7 @@ public class Player : MonoBehaviour
     void Update()
     {
         ViewRotation();
+        TakeObject();
     }
 
     void FixedUpdate()
@@ -43,27 +48,36 @@ public class Player : MonoBehaviour
         inputDir.x += Input.GetAxis("Mouse X") * Time.deltaTime * sensitivity;
         inputDir.y -= Input.GetAxis("Mouse Y") * Time.deltaTime * sensitivity;
 
-        Quaternion viewRotation = Quaternion.Euler(Mathf.Clamp(inputDir.y, -70, 80), inputDir.x, 0f);
         Quaternion bodyRotation = Quaternion.Euler(0f, inputDir.x, 0f);
-
+        Quaternion viewRotation = Quaternion.Euler(Mathf.Clamp(inputDir.y, -70, 80), inputDir.x, 0f);
+        
         view.transform.rotation = Quaternion.Slerp(view.transform.rotation, viewRotation, smoothTime);
         transform.rotation = Quaternion.Slerp(transform.rotation, bodyRotation, smoothTime);
-
-        /*currentRotationX = view.transform.rotation.x;
-        currentRotationY = transform.rotation.y;
-
-        var tmp = Mathf.Clamp(currentRotationY + inputDir.y * Time.deltaTime * sensitivity, -80, 70);
-        var rot = tmp - currentRotationY;
-        view.transform.Rotate(-rot, 0f, 0f);
-
-        transform.Rotate(0f, inputDir.x * Time.deltaTime * sensitivity, 0f);*/
     }
 
     void TakeObject()
     {
+        RaycastHit hit;
+
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
+            if (Physics.Raycast(view.transform.position, view.transform.forward, out hit, maxTakeDistance, part))
+            {
+                objectRB = hit.transform.gameObject.GetComponent<Rigidbody>();
+                takeMode = true;
+                Debug.Log("Take " + hit.transform.gameObject.name);
+            }
+        }
 
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            takeMode = false;
+            Debug.Log("Drop ");
+        }
+
+        if (takeMode)
+        {
+            objectRB.AddForce((inputDir - view.transform.forward) * force);
         }
     }
 
